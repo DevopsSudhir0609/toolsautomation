b978e5749dfb3a3ea127df9188de5f722f9d0b2d
sudir eagala <sudireagala@vvhitsolutions.com>
2025-01-16 21:04:01 +1000

    tools automation

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "project-Alpha"
+    resource_group_name  = "Project-Alpha"
     storage_account_name = "projectalpha13"
     container_name       = "toolsautomation"
     key                  = "terraform.tfstate"
